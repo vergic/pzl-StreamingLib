06209ead0d0f135b8b89ffaec9ff9ad10a57f631
Tobias Lind <tobias@vergic.com>
2022-09-15 15:33:04 +0200

    Updated dependencies Changed "main" entry point in package.json Renamed lib to "pzlStreamLib"

--- src/VngageStream_deps.js
+++ src/VngageStream_deps.js
@@ -3,7 +3,7 @@ import StreamEventTypes from './StreamEventTypes';
 import StreamErrors from './StreamErrors';
 import Subscription from './Subscription';
 
-export default {
+export {
     StreamEventTypes,	// Is this needed? Or could it be required/imported directly?
     StreamErrors,	// Is this needed? Or could it be required/imported directly?
     StreamConnector,
